922344e0d51cce3f165348530d1e27a067d50d5f
Lawrence-oss <lawrence.ejiofor@Ethnoscyber.com>
2026-02-10 14:28:33 +0100

    Initial Html and css structue and definining the questions

---
+++ script.js
@@ -0,0 +1,15 @@
+const question = [
+  {
+     question: "What is the capital of France?",
+      options: ["Berlin","Madrid", "Paris", "Rome"],    
+      answer: 2
+    },
+    {
+       question: "Which planet is closest to the Sun?",
+      options: ["Venus","Mercury", "Earth", "Mars"],   
+      answer: 1
+    }
+]
+
+let currentQuestion = 0;
+let score = 0;
