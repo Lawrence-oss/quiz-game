9e3d7aaa39f98ecdbdc713be463f4e45bcfca6b7
Lawrence-oss <lawrence.ejiofor@Ethnoscyber.com>
2026-02-11 12:49:14 +0100

    Added the Show Question function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const question = [
+const questions = [
   {
      question: "What is the capital of France?",
       options: ["Berlin","Madrid", "Paris", "Rome"],    
@@ -13,3 +13,22 @@ const question = [
 
 let currentQuestion = 0;
 let score = 0;
+
+const questionElement = document.getElementById("question");
+const optionsContainer = document.getElementById("options");
+const scoreElement = document.getElementById("score");
+const resultElement = document.getElementById("result");
+const restartBtn = document.getElementById("restart-btn");
+
+function showQuestion() {
+  const current = questions[currentQuestion];
+  questionElement.textContent = current.question;
+  optionsContainer.innerHTML = "";
+  current.options.forEach((optionText, index) => {
+    const btn = document.createElement("button");
+    btn.textContent = optionText;
+    optionsContainer.appendChild(btn);
+})
+}
+
+showQuestion()
